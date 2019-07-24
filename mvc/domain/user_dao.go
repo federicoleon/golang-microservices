ad2cd6023934bc21cb3bba58da4853f56c8b3f47
Federico León <fedeleon.cba@gmail.com>
2019-07-24 01:08:02 -0300

    Unit tests

--- mvc/domain/user_dao.go
+++ mvc/domain/user_dao.go
@@ -18,7 +18,7 @@ func GetUser(userId int64) (*User, *utils.ApplicationError) {
 	}
 
 	return nil, &utils.ApplicationError{
-		Message:    fmt.Sprintf("user %v was not found", userId),
+		Message:    fmt.Sprintf("user %v does not exists", userId),
 		StatusCode: http.StatusNotFound,
 		Code:       "not_found",
 	}
---
+++ mvc/domain/user_dao_test.go
@@ -0,0 +1,34 @@
+package domain
+
+import (
+	"testing"
+	"net/http"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetUserNoUserFound(t *testing.T) {
+	// Initialization:
+
+	// Execution:
+	user, err := GetUser(0)
+
+	// Validation:
+	assert.Nil(t, user, "we were not expecting a user with id 0")
+	assert.NotNil(t, err)
+
+	assert.EqualValues(t, http.StatusNotFound, err.StatusCode)
+	assert.EqualValues(t, "not_found", err.Code)
+	assert.EqualValues(t, "user 0 does not exists", err.Message)
+}
+
+func TestGetUserNoError(t *testing.T) {
+	user, err := GetUser(123)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, user)
+
+	assert.EqualValues(t, 123, user.Id)
+	assert.EqualValues(t, "Fede", user.FirstName)
+	assert.EqualValues(t, "Leon", user.LastName)
+	assert.EqualValues(t, "myemail@gmail.com", user.Email)
+}
